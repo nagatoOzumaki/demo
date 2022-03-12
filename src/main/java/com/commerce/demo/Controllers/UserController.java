bd33449430d9a40349fd53fa072fe74d9d108e41
riad <riad@gmail.com>
2022-03-12 17:03:35 +0100

    "no change"

--- src/main/java/com/commerce/demo/Controllers/UserController.java
+++ src/main/java/com/commerce/demo/Controllers/UserController.java
@@ -1,22 +1,13 @@
 package com.commerce.demo.Controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-
-import com.commerce.demo.Models.Customer;
-import com.commerce.demo.Models.Order;
 import com.commerce.demo.Models.Product;
-import com.commerce.demo.Repositories.ProductRepository;
 import com.commerce.demo.Services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping(value = "/site/v1/user")
@@ -32,7 +23,7 @@ public class UserController extends MainController {
 
     @GetMapping(value = "/show_details/{product_id}")
     public Product show_product_details(@PathVariable int product_id) {
-        return userService.show_product_details(product_id)
+        return userService.show_product_details(product_id);
 
     }
 
---
+++ src/main/java/com/commerce/demo/Security/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.commerce.demo.Security;
+
+public class SecurityConfig {
+
+}
