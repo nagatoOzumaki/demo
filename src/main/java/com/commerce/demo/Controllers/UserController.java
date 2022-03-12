f3210ea3f63c2252a09826893f2df4f01878fe88
riad <riad@gmail.com>
2022-03-12 16:55:54 +0100

    userService changed

--- src/main/java/com/commerce/demo/Controllers/UserController.java
+++ src/main/java/com/commerce/demo/Controllers/UserController.java
@@ -8,7 +8,9 @@ import com.commerce.demo.Models.Customer;
 import com.commerce.demo.Models.Order;
 import com.commerce.demo.Models.Product;
 import com.commerce.demo.Repositories.ProductRepository;
+import com.commerce.demo.Services.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,20 +21,19 @@ import jakarta.websocket.server.PathParam;
 @RestController
 @RequestMapping(value = "/site/v1/user")
 public class UserController extends MainController {
+    @Autowired
+    UserService userService;
+
     @GetMapping(value = "/order_product/{customer_id}/{product_id}")
     public void order_product(@PathVariable int product_id, @PathVariable int customer_id) {
-        Product product = productRepository.findById(product_id).get();
-        Customer customer = customerRepository.findById(customer_id).get();
-        Order order = new Order(product, customer);
-        orderRepository.save(order);
+        userService.order_product(product_id, customer_id);
 
     }
 
     @GetMapping(value = "/show_details/{product_id}")
     public Product show_product_details(@PathVariable int product_id) {
-        Product product = productRepository.findById(product_id).get();
-        return product;
+        return userService.show_product_details(product_id)
 
     }
-    
+
 }
--- src/main/java/com/commerce/demo/Services/UserService.java
+++ src/main/java/com/commerce/demo/Services/UserService.java
@@ -1,10 +1,22 @@
 package com.commerce.demo.Services;
 
+import com.commerce.demo.Models.Customer;
 import com.commerce.demo.Models.Order;
+import com.commerce.demo.Models.Product;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService extends MainService {
 
+    public void order_product(int product_id, int customer_id) {
+        Product product = productRepository.findById(product_id).get();
+        Customer customer = customerRepository.findById(customer_id).get();
+        Order order = new Order(product, customer);
+        orderRepository.save(order);
+    }
+
+    public Product show_product_details(int product_id) {
+        return productRepository.findById(product_id).get();
+    }
 }
